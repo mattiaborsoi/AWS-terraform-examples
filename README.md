fa7b0a20043e107f9d9b951803378a5758ff6560
Mattia Borsoi <mattia.borsoi@gmail.com>
2020-02-27 11:03:34 +0000

    Initial commit
    single-web-server and cluster-of-web-servers

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# AWS-terraform-examples
- Playing with Terraform to deploy AWS instances
+ ![Terraform Version](https://img.shields.io/badge/tf-%3E%3D0.12.0-blue.svg)
+
+ #  Playing with Terraform to deploy AWS instances
+
+ ## Quick start
+
+ **Note**: These examples deploy resources into your AWS account. Although all the resources should fall under the
+ [AWS Free Tier](https://aws.amazon.com/free/), it is not my responsibility if you are charged money for this.
+
+ 1. Install [Terraform](https://www.terraform.io/).
+ 1. Set your AWS credentials as the environment variables `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`.
+ 1. `cd` into one of the example folders.
+ 1. Run `terraform init`.
+ 1. Run `terraform apply`.
+ 1. After it's done deploying, the example will output URLs or IPs you can try out.
+ 1. To clean up and delete all resources after you're done, run `terraform destroy`.
+
+
+ ## Examples
+
+
+  1. [single-web-server](./single-web-server): Deploy a single EC2 Instance with a web server that will return
+        "Hello, World" for every request on port 8080.
+  1. [cluster-of-web-servers](./cluster-of-web-servers): Deploy a cluster of EC2 Instances in an Auto Scaling Group
+        (ASG) and an Elastic Load Balancer (ELB). The ELB listens on port 80 and distributes load across the EC2
+        Instances, each of which runs the same "Hello, World" web server.
+  1. [s3-backend](./s3-backend): Create an S3 bucket and DynamoDB table to use as a Terraform backend.
+  1. [database](./database): Deploy MySQL on top of Amazon's Relational Database Service (RDS).
+  1. [modules](./modules): Examples of reusable Terraform modules, including a module that can deploy a web server
+        cluster on top of ASG with an ELB.
+  1. [live](./live): Examples of how to deploy different live environments (i.e., staging, production) using the code
+        from the `modules` folder.
+  1. [loops-with-count](./loops-with-count): Examples of how to use the `count` parameters to "loop" over resources.        
+  1. [loops-with-for-each](./loops-with-for-each): Examples of how to use `for_each` to "loop" over inline blocks.        
+  1. [loops-with-for](./loops-with-for): Examples of how to use `for` to "loop" over individual values.        
---
+++ single-web-server/main.tf
@@ -0,0 +1,58 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DEPLOY A SINGLE EC2 INSTANCE
+# This template runs a simple "Hello, World" web server on a single EC2 Instance
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+# ----------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+# ------------------------------------------------------------------------------
+# CONFIGURE OUR AWS CONNECTION
+# ------------------------------------------------------------------------------
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY A SINGLE EC2 INSTANCE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_instance" "example" {
+  # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in eu-west-2
+  ami                    = "ami-006a0174c6c25ac06"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE THE SECURITY GROUP THAT'S APPLIED TO THE EC2 INSTANCE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
