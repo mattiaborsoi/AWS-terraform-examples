111f4d7f8842fb975595629f4733f85ec86877f0
Mattia Borsoi <mattia.borsoi@gmail.com>
2020-02-27 15:51:22 +0000

    adding more examples

--- live/prod/services/webserver-cluster/main.tf
+++ live/prod/services/webserver-cluster/main.tf
@@ -1,5 +1,5 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# CREATE AN S3 BUCKET AND DYNAMODB TABLE TO USE AS A TERRAFORM BACKEND
+# DEPLOY A WEBSERVER CLUSTER USING THE WEBSERVER-CLUSTER MODULE
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -20,40 +20,14 @@ provider "aws" {
 }
 
 # ------------------------------------------------------------------------------
-# CREATE THE S3 BUCKET
+# DEPLOY THE WEBSERVER-CLUSTER MODULE
 # ------------------------------------------------------------------------------
 
-resource "aws_s3_bucket" "terraform_state" {
-  # TODO: change this to your own name! S3 bucket names must be *globally* unique.
-  bucket = "mattiaborsoi-terraform-up-and-running-state"
+module "webserver_cluster" {
+  source = "../../../../modules/services/webserver-cluster"
 
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
-
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-# ------------------------------------------------------------------------------
-# CREATE THE DYNAMODB TABLE
-# ------------------------------------------------------------------------------
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+  cluster_name  = "webservers-prod"
+  instance_type = "m4.large"
+  min_size      = 2
+  max_size      = 10
 }
--- loops-with-count/main.tf
+++ loops-with-count/main.tf
@@ -1,5 +1,5 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# CREATE AN S3 BUCKET AND DYNAMODB TABLE TO USE AS A TERRAFORM BACKEND
+# SHOW AN EXAMPLE OF LOOPS USING THE COUNT PARAMETER BY CREATING MULTIPLE IAM USERS
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -20,40 +20,10 @@ provider "aws" {
 }
 
 # ------------------------------------------------------------------------------
-# CREATE THE S3 BUCKET
+# CREATE MULTIPLE IAM USERS USING COUNT
 # ------------------------------------------------------------------------------
 
-resource "aws_s3_bucket" "terraform_state" {
-  # TODO: change this to your own name! S3 bucket names must be *globally* unique.
-  bucket = "mattiaborsoi-terraform-up-and-running-state"
-
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
-
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-# ------------------------------------------------------------------------------
-# CREATE THE DYNAMODB TABLE
-# ------------------------------------------------------------------------------
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
 }
